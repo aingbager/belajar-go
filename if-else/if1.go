760a807bdb4902a8756c7cf1fa958a618b71fc59
aingbager <aingbager55@gmail.com>
2023-06-13 00:50:42 +0700

    add if

---
+++ if-else/if1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	angka := 2
+
+	if angka == 2 {
+		fmt.Println("ini angka 2")
+	} else if angka == 6 {
+		fmt.Println("ini ini angka 6")
+	} else {
+		fmt.Println("bukan angka yang dicari")
+	}
+}
